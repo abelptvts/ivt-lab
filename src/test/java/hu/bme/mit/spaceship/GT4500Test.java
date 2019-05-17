1287d3f6c20a612ce7352dce2d41825c6247fff8
Abel Putovits <abelputovits@debian-putovits>
2019-05-17 09:21:23 +0200

    add unit tests to increase coverage to 100%

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -1,40 +1,137 @@
 package hu.bme.mit.spaceship;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import static org.mockito.Mockito.*;
 
 public class GT4500Test {
 
-  private GT4500 ship;
+  @Mock TorpedoStore primaryTorpedoStore;
+  @Mock TorpedoStore secondaryTorpedoStore;
+  @InjectMocks private GT4500 ship;
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    MockitoAnnotations.initMocks(this);
   }
 
   @Test
-  public void fireTorpedo_Single_Success(){
-    // Arrange
-
-    // Act
-    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
-
-    // Assert
-    assertEquals(true, result);
+  public void shouldFirePrimaryTorpedoStore() {
+    when(primaryTorpedoStore.isEmpty()).thenReturn(false);
+    when(primaryTorpedoStore.fire(1)).thenReturn(true);
+    assertEquals(true, ship.fireTorpedo(FiringMode.SINGLE));
   }
 
   @Test
-  public void fireTorpedo_All_Success(){
-    // Arrange
-
-    // Act
-    boolean result = ship.fireTorpedo(FiringMode.ALL);
-
-    // Assert
-    assertEquals(true, result);
+  public void shouldFireSecondaryTorpedo() {
+    when(primaryTorpedoStore.isEmpty()).thenReturn(true);
+    when(secondaryTorpedoStore.isEmpty()).thenReturn(false);
+    when(secondaryTorpedoStore.fire(1)).thenReturn(true);
+    assertEquals(true, ship.fireTorpedo(FiringMode.SINGLE));
   }
 
+  @Test
+  public void shouldNotFireTorpedo() {
+    when(primaryTorpedoStore.isEmpty()).thenReturn(true);
+    when(secondaryTorpedoStore.isEmpty()).thenReturn(true);
+    assertEquals(false, ship.fireTorpedo(FiringMode.SINGLE));
+  }
+
+  @Test
+  public void shouldFireAlternating() {
+    when(primaryTorpedoStore.isEmpty()).thenReturn(false);
+    when(secondaryTorpedoStore.isEmpty()).thenReturn(false);
+    when(primaryTorpedoStore.fire(1)).thenReturn(true);
+    when(secondaryTorpedoStore.fire(1)).thenReturn(true);
+    assertEquals(true, ship.fireTorpedo(FiringMode.SINGLE));
+    assertEquals(true, ship.fireTorpedo(FiringMode.SINGLE));
+  }
+
+  @Test
+  public void shouldFirePrimaryOnly() {
+    when(primaryTorpedoStore.isEmpty()).thenReturn(false);
+    when(secondaryTorpedoStore.isEmpty()).thenReturn(true);
+    when(primaryTorpedoStore.fire(1)).thenReturn(true);
+    assertEquals(true, ship.fireTorpedo(FiringMode.ALL));
+  }
+
+  // based on code
+  @Test
+  public void shouldNotFireAtAll() {
+    when(primaryTorpedoStore.isEmpty()).thenReturn(true);
+    when(secondaryTorpedoStore.isEmpty()).thenReturn(true);
+    assertEquals(false, ship.fireTorpedo(FiringMode.ALL));
+  }
+
+  @Test
+  public void shouldNotFireLaser() {
+    assertEquals(false, ship.fireLaser(FiringMode.ALL));
+    assertEquals(false, ship.fireLaser(FiringMode.SINGLE));
+  }
+
+  @Test
+  public void shouldFirePrimaryTwice() {
+    when(primaryTorpedoStore.isEmpty()).thenReturn(false);
+    when(secondaryTorpedoStore.isEmpty()).thenReturn(true);
+    when(primaryTorpedoStore.fire(1)).thenReturn(true);
+    ship.fireTorpedo(FiringMode.SINGLE);
+    assertEquals(true, ship.fireTorpedo(FiringMode.SINGLE));
+  }
+
+  @Test
+  public void shouldFireAllTorpedos() {
+    when(primaryTorpedoStore.isEmpty()).thenReturn(false);
+    when(secondaryTorpedoStore.isEmpty()).thenReturn(false);
+    when(primaryTorpedoStore.fire(1)).thenReturn(true);
+    when(secondaryTorpedoStore.fire(1)).thenReturn(true);
+    assertEquals(true, ship.fireTorpedo(FiringMode.ALL));
+  }
+
+  @Test
+  public void shouldFireAllTorpedosStartingWithSecondary() {
+    when(primaryTorpedoStore.isEmpty()).thenReturn(false);
+    when(secondaryTorpedoStore.isEmpty()).thenReturn(false);
+    when(primaryTorpedoStore.fire(1)).thenReturn(true);
+    when(secondaryTorpedoStore.fire(1)).thenReturn(true);
+    assertEquals(true, ship.fireTorpedo(FiringMode.SINGLE));
+    assertEquals(true, ship.fireTorpedo(FiringMode.ALL));
+  }
+
+  @Test
+  public void shouldFailWhenTryingToFirePrimaryTwice() {
+    when(primaryTorpedoStore.isEmpty()).thenReturn(false);
+    when(primaryTorpedoStore.fire(1)).thenReturn(true);
+    when(secondaryTorpedoStore.isEmpty()).thenReturn(true);
+    ship.fireTorpedo(FiringMode.SINGLE);
+    when(primaryTorpedoStore.isEmpty()).thenReturn(true);
+    assertEquals(false, ship.fireTorpedo(FiringMode.SINGLE));
+  }
+
+  @Test
+  public void shouldNotFireAtAll_primaryLast() {
+    when(primaryTorpedoStore.isEmpty()).thenReturn(false);
+    when(primaryTorpedoStore.fire(1)).thenReturn(true);
+    ship.fireTorpedo(FiringMode.SINGLE);
+    when(primaryTorpedoStore.isEmpty()).thenReturn(true);
+    when(secondaryTorpedoStore.isEmpty()).thenReturn(true);
+    assertEquals(false, ship.fireTorpedo(FiringMode.ALL));
+  }
+
+  @Test
+  public void shouldFireSecondaryOnly_primarylast() {
+    when(primaryTorpedoStore.isEmpty()).thenReturn(false);
+    when(primaryTorpedoStore.fire(1)).thenReturn(true);
+    ship.fireTorpedo(FiringMode.SINGLE);
+    when(primaryTorpedoStore.isEmpty()).thenReturn(true);
+    when(secondaryTorpedoStore.isEmpty()).thenReturn(false);
+    when(secondaryTorpedoStore.fire(1)).thenReturn(true);
+    assertEquals(true, ship.fireTorpedo(FiringMode.ALL));
+  }
 }
